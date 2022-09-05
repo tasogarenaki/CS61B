c3cd1cb3a657eba2b28f52681a490da63cd8ca23
tasogarenaki <terry.chan2h@gmail.com>
2022-09-05 21:28:58 +0200

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
-        int x = 0;
+        int x = 0, y = 0;
         while (x < 10) {
-            System.out.print(x + " ");
+            System.out.println(y + " ");
             x = x + 1;
+            y = y + x;
         }
     }
 }
