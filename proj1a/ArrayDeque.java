e0bbb648733fcf471a8c4b687c9f16bfeb9ed3a3
tasogarenaki <terry.chan2h@gmail.com>
2022-11-20 16:06:27 +0100

    finished arraydeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,26 +6,28 @@
  *
  * Uses a circular array with 2 pointers.
  * 1) The initial length of array should be 8.
- * 2) If the Array is full, its size is increassed by growthFactor.
- * 3) If the array's length is greater or equal to 16 and (the number of items / the array's length) is
- *    smaller than the usageFacor (25%), array is reduced.
+ * 2) If the Array is full, its size should increassed by GROWTH_FACTOR.
+ * 3) If the array's length is greater or equal to 16 and
+ *    (the number of elements / the array's length) is
+ *    smaller than the usageFacor (25%), then array is reduced.
  */
 public class ArrayDeque<T> {
 
-    private static final int initialLength = 8;
-    private static final int growthFactor = 2;
-    private static final double usageFactor = 0.25;
+    private static final int INITIAL_LENGTH = 8;
+    private static final int GROWTH_FACTOR = 2;
+    private static final double USAGE_FACTOR = 0.25;
 
-    private T[] items;
+    private T[] elements;
     private int head;   // head pointer at first item
     private int tail;   // end pointer at last item
     private int size;   // current length
 
     /**
-     * Creates an empty array deque.
+     * Creates an empty array deque with head and tail start
+     * at the same position 0.
      */
     public ArrayDeque() {
-        items = (T[]) new Object[initialLength];
+        elements = (T[]) new Object[INITIAL_LENGTH];
         head = 0;
         tail = 0;
         size = 0;
@@ -33,134 +35,142 @@ public class ArrayDeque<T> {
 
     /**
      * Adds an item to the head of the deque.
-     * @param item the item to be added at the beginning of the deque.
+     * @param item is the item to be added at the front of the deque.
      */
     public void addFirst(T item) {
-        /* circular, to located the position of the head pointer.
-         * e.g. if items.length = 4 and head = 0 then 3 % 4 = 3,
-         * so item adds on position 3 (head points the end of array),
-         * and every time adds an item will started from back n-1, n-2 and so on. */
-        head = (head - 1 + items.length) % items.length;
-        items[head] = item;
+        /*
+         * circular, to locate the position of the head pointer.
+         * e.g. if element.length = 4 and head = 0 then 3 % 4 = 3,
+         * so item adds on position 3 (head points at the end of array),
+         * and every time an item is added, it will start from back n-1, n-2 and so on.
+         */
+        head = (head - 1 + elements.length) % elements.length;
+        elements[head] = item;
         size++;
-        /* Change the capacity of the array */
-        //if (size == items.length) {
+
+        /* Change the capacity of the array. */
         if (head == tail) {
-            doubleSize(size * growthFactor);
+            calculateSize(size * GROWTH_FACTOR);
         }
     }
 
     /**
      * Adds an item to the back of the deque.
-     * @param item the item to be added at the end of the deque.
+     * @param item is the item to be added at the end of the deque.
      */
     public void addLast(T item) {
+        elements[tail] = item;
+        /* circular adds the tail's position by 1. */
+        tail = (tail + 1) % elements.length;
+        size++;
 
-
+        if (head == tail) {
+            calculateSize(size * GROWTH_FACTOR);
+        }
     }
 
     /**
-     * @return ture if deque is empty, false otherwise
+     * @return true if deque is empty, false otherwise.
      */
     public boolean isEmpty() {
-        return true;
-
+        return size == 0;
     }
 
     /**
-     * @return the number of items in the deque.
+     * @return the number of elements in the deque.
      */
     public int size() {
         return size;
-
     }
 
     /**
-     * Prints the items in the deque from first to last, separated by a space.
+     * Prints the elements in the deque from first to last, separated by a space.
      */
     public void printDeque() {
-
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) + " ");
+        }
     }
 
     /**
-     * Removes and returns the item at the front of the deque.
-     * Uses helper function remove() with input sentinel.next
+     * Removes and returns the first item in the deque.
      * @return the first item in the deque, if no such item exists, returns null.
      */
     public T removeFirst() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
 
+        /* Gets the value of the head element. */
+        T item = elements[head];
+        elements[head] = null;
+        /*
+         * After removing the value of the head element,
+         * the head pointer's position should move (+1).
+         */
+        head = (head + 1) % elements.length;
+        size--;
+
+        /* Reduce the array to save the memory. */
+        if (size >= 2 * INITIAL_LENGTH && size / elements.length < USAGE_FACTOR) {
+            calculateSize(elements.length / GROWTH_FACTOR);
+        }
+        return item;
     }
 
     /**
-     * Removes and returns the item at the back of the deque.
-     * Uses helper function with input sentinel.prev
+     * Removes and returns the item to the back of the deque.
      * @return the last item in the deque, if no such item exists, returns null.
      */
     public T removeLast() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
 
+        /* Gets the value of the tail element. */
+        T item = elements[tail];
+        elements[tail] = null;
+        /*
+         * After removing the value at the tail element,
+         * the tail pointer's position should move (-1).
+         */
+        tail = (tail - 1 + elements.length) % elements.length;
+        size--;
+
+        /* Reduce the array to save the memory. */
+        if (size >= 2 * INITIAL_LENGTH && size / elements.length < USAGE_FACTOR) {
+            calculateSize(elements.length / GROWTH_FACTOR);
+        }
+        return item;
     }
 
     /**
-     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-     * @param index of the item to be returned
+     * Gets the item from the given index, which starts after head pointer,
+     * because the head pointer is where the array starts.
+     * @param index of the item to be returned (!= pointer's position)
      * @return      the item at the given index, or null if no such item exists
      */
     public T get(int index) {
-        return null;
+        if (isEmpty() || index > size) {
+            return null;
+        }
 
+        int theIndex = (head + index) % elements.length;
+        return elements[theIndex];
     }
-
-    /**
-     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-     * Uses recursion with helper function
-     * @param index of the item to be returned
-     * @return      the item at the given index, or null if no such item exists
-     */
-    public T getRecursive(int index) {
-        return null;
-
-    }
-
-    /**
-     * Removes whether the first or last node and returns its value.
-     * @param node node to be removed
-     * @return     the value of the removed node
-     */
-    // private T remove(LinkedListDeque.Node<T> node) {
-    //     return null;
-    // }
-
-    /**
-     * Gets the item at the given index.
-     * @param index of the item to be returned
-     * @param n     the node to count
-     * @return      the value at the node
-     */
-    // private T helperGetRecursive(int index, LinkedListDeque.Node<T> n) {
-    //     return null;
-    //
-    // }
 
     /**
      * Double the size of the array with newSize, if array is full,
      * which is head equals tail.
      * @param newSize is the new length of the array.
      */
-    private void doubleSize(int newSize) {
+    private void calculateSize(int newSize) {
         T[] resized = (T[]) new Object[newSize];
-        /* Copy Array */
-        // for (int i = 0; i < size; i++) {
-        //     int src = (head + i) % size;
-        //     int desk = i;
-        //     resized[desk] = items[src];
-        // }
-        /* Copy left side items of head */
-        System.arraycopy(items, head, resized, 0, items.length - head);
-        /* Copy right side items of head */
-        System.arraycopy(items, 0, resized, items.length - head, head);
-        items = resized;
+        /* Copy left side elements of head. */
+        System.arraycopy(elements, head, resized, 0, elements.length - head);
+        /* Copy right side elements of head. */
+        System.arraycopy(elements, 0, resized, elements.length - head, head);
+        elements = resized;
         head = 0;
         tail = size;
     }
