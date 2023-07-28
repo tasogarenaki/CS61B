dd10bdc5312d495bd0b21e5c88ebea5f7dc1e0e3
tasogarenaki <terry.chan2h@gmail.com>
2023-07-28 11:37:48 +0200

    generate rooms

--- proj2/byog/Core/MapGenerator.java
+++ proj2/byog/Core/MapGenerator.java
@@ -13,8 +13,17 @@ import java.util.Random;
 import java.awt.*;
 
 public class MapGenerator {
-    public static final int WIDTH = 80;
-    public static final int HEIGHT = 30;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 30;
+
+    private static final int ROOMMAXLEN = 8;
+
+    private static final int MIN_X = 0;
+    private static final int MIN_Y = 0;
+
+    private static final int ROOM_LIMIT = 200;   // maximum number of rooms
+
+
 
 
     /* Set font size to displayed. */
@@ -24,17 +33,7 @@ public class MapGenerator {
 
 
 
-    protected static class Coordinate {
-        int x;
-        int y;
 
-        Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-
-    }
 
     private static class World {
         private List<Room> rooms;
@@ -48,6 +47,46 @@ public class MapGenerator {
         }
     }
 
+
+    private static class Region {
+        List<Connect> connects;
+        boolean connected;
+    }
+
+    private static class Room extends Region {
+        // TODO: generate rooms
+
+    }
+
+
+    protected static class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * Connects the rooms.
+     */
+    private static class Connect {
+        Coordinate coord;
+        Region connectTo;
+
+        Connect(Coordinate coord, Region connectTo) {
+            this.coord = coord;
+            this.connectTo = connectTo;
+        }
+    }
+
+
+
+
+
+
     public static TETile[][] generateWorld(Random rand) {
         World world = new World();
         TETile[][] map = world.map;
@@ -58,15 +97,57 @@ public class MapGenerator {
                 map[x][y] = Tileset.NOTHING;
             }
         }
+        // TODO: generate the world with rooms and connect them.
 
 
-        return null;
+        return world.map;
     }
+
+    private static void generateRoom(World world, Random rand) {
+        for (int i = 0; i < ROOM_LIMIT; i++) {
+            /* Set room size. */
+            int botLeftX = RandomUtils.uniform(rand, MIN_X, WIDTH);
+            int botLeftY = RandomUtils.uniform(rand, MIN_Y, HEIGHT);
+            int topRightX = 2 * RandomUtils.uniform(rand, (botLeftX + 1) / 2,
+                    Integer.min(WIDTH, botLeftX + ROOMMAXLEN) / 2);
+            int topRightY = 2 * RandomUtils.uniform(rand, (botLeftY + 1) / 2,
+                    Integer.min(HEIGHT, botLeftY + ROOMMAXLEN) / 2);
+
+            /* Generate World via Coordinates. */
+            Room rand = new Room(new Coordinate(botLeftX, botLeftY),
+                    new Coordinate(topRightX, topRightY));
+
+            if (!isRoomOverlap(world, rand)) {
+                world.rooms.add(rand);
+            }
+        }
+        fillRoom(world);
+    }
+
+
+
+
+
+    // TODO: Check if is new room is overlapped
+
+
+
+    // TODO: fill the room
+
+
+
+
+
+
+
+
+
 
 
     protected static Coordinate getPlayer() {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < WIDTH; y++) {
+                // TODO: find the player and return it
 
             }
         }
@@ -74,6 +155,14 @@ public class MapGenerator {
         return null;
 
     }
+
+
+
+
+
+
+
+
 
 
 
