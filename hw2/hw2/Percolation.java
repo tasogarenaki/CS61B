560cd06efdbe28a29c640238f06cf87235f608bb
tasogarenaki <terry.chan2h@gmail.com>
2023-09-06 15:33:21 +0200

    start hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,22 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * Percolation
+ * Model of a percolation system using an N-by-N grid of sites.
+ * Each site is either open or blocked. A full site is an open site that can be connected
+ * to an open site in the top row via a chain of neighboring (left, right, up, down) open sites.
+ * The system percolates if there is a full site in the bottom row.
+ *
+ * @author Terry
+ */
 public class Percolation {
 
+    public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
+    public void open(int row, int col)       // open the site (row, col) if it is not open already
+    public boolean isOpen(int row, int col)  // is the site (row, col) open?
+    public boolean isFull(int row, int col)  // is the site (row, col) full?
+    public int numberOfOpenSites()           // number of open sites
+    public boolean percolates()              // does the system percolate?
+    public static void main(String[] args)   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,19 @@
 package hw2;
 
+/**
+ * PercolationStats
+ * Estimates the percolation threshold (mu) using the Monte Carlo Method.
+ * @author Terry
+ */
 public class PercolationStats {
-
+    /* perform T independent experiments on an N-by-N grid. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Grid size must be a natural number.");
+        }
+    }
+    public double mean()                                           // sample mean of percolation threshold
+    public double stddev()                                         // sample standard deviation of percolation threshold
+    public double confidenceLow()                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh()                                 // high endpoint of 95% confidence interval
 }
