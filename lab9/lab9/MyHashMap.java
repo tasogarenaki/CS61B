f10c0381773d58af84e7d257b199e54a5161efc4
tasogarenaki <terry.chan2h@gmail.com>
2023-09-06 11:38:11 +0200

    finished lab9

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -7,7 +8,7 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Terry
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -17,13 +18,13 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     private ArrayMap<K, V>[] buckets;
     private int size;
 
-    private int loadFactor() {
-        return size / buckets.length;
-    }
-
     public MyHashMap() {
         buckets = new ArrayMap[DEFAULT_SIZE];
         this.clear();
+    }
+
+    private int loadFactor() {
+        return size / buckets.length;
     }
 
     /* Removes all of the mappings from this map. */
@@ -53,19 +54,50 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            return null;
+        }
+
+        return buckets[hash(key)].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            return;
+        }
+        size = buckets[hash(key)].containsKey(key) ? size : size + 1;
+        buckets[hash(key)].put(key, value);
+
+        if (loadFactor() > MAX_LF) {
+            resize(buckets.length * 2);
+        }
+    }
+
+    /**
+     * Resizes the array of ArrayMap.
+     * @param capacity  new size
+     */
+    private void resize(int capacity) {
+        ArrayMap<K, V>[] old_buckets = buckets;
+        Set<K> keys = keySet();
+
+        buckets = new ArrayMap[capacity];
+        clear();
+
+        for (K key : keys) {
+            int old_hash = Math.floorMod(key.hashCode(), old_buckets.length);
+            V value = old_buckets[old_hash].get(key);
+            buckets[hash(key)].put(key, value);
+            size++;
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -73,7 +105,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> key_set = new HashSet<>();
+        for (ArrayMap<K, V> bucket : buckets) {
+            key_set.addAll(bucket.keySet());
+        }
+        return key_set;
     }
 
     /* Removes the mapping for the specified key from this map if exists.
@@ -81,7 +117,12 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = buckets[hash(key)].remove(key);
+
+        if (size < MAX_LF / 4) {
+            resize(buckets.length / 2);
+        }
+        return value;
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -89,11 +130,16 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (get(key).equals(value)) {
+            return remove(key);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        Set<K> key_set = keySet();
+        return key_set.iterator();
     }
 }
