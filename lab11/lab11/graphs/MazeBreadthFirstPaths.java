88fc04a67c895e81fcedd1921dff2bea5d545e62
tasogarenaki <terry.chan2h@gmail.com>
2023-09-14 16:47:55 +0200

    finished BFS

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,6 +1,10 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
+ * Implements a BFS for a Maze represented as an undirected graph.
  *  @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
@@ -10,20 +14,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    /* A queue for implementing BFS. */
+    private Queue<Integer> fringe;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        fringe = new LinkedList<>();
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        int v = s;
+        fringe.add(v);
+        targetFound = (v == t);
+        while (!targetFound && !fringe.isEmpty()) {
+            v = fringe.remove();
+            marked[v] = true;
+            announce();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    targetFound = (v == t);
+                    fringe.add(w);
+                }
+                if (targetFound) {
+                    return;
+                }
+            }
+        }
     }
-
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
