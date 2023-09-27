7b93ec2669371b0bc7cbe2b06207016147ef3cdb
tasogarenaki <terry.chan2h@gmail.com>
2023-09-27 17:16:11 +0200

    finished task1

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -1,7 +1,25 @@
 package lab14;
 
+import lab14lib.*;
+
 public class Main {
 	public static void main(String[] args) {
-		/** Your code here. */
+		/*
+		Generator generator = new SineWaveGenerator(40);
+		GeneratorPlayer gp = new GeneratorPlayer(generator);
+		gp.play(1000000);
+
+		Generator generator = new SineWaveGenerator(200);
+		GeneratorDrawer gd = new GeneratorDrawer(generator);
+		gd.draw(4096);
+
+		Generator generator = new SineWaveGenerator(200);
+		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
+		gav.drawAndPlay(4096, 1000000);
+		*/
+
+		Generator generator = new SawToothGenerator(512);
+		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
+		gav.drawAndPlay(4096, 1000000);
 	}
-} 
+}
