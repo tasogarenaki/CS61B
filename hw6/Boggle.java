25bbb9544ed6a9ded3a0cec9b6d0498c09343c88
tasogarenaki <terry.chan2h@gmail.com>
2023-10-20 15:10:48 +0200

    start hw6

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -16,7 +16,32 @@ public class Boggle {
      *         have them in ascending alphabetical order.
      */
     public static List<String> solve(int k, String boardFilePath) {
-        // YOUR CODE HERE
+        if (k <= 0) {
+            throw new IllegalArgumentException("k is non-positive");
+        }
+
+        In dict = new In(dictPath);
+        String[] words = dict.readAllLines();
+        Trie dictionary = Trie.create(words);
+
+        In board = new In(boardFilePath);
+        String[] boardLines = board.readAllLines();
+        int expectedLineLength = boardLines[0].length();
+        char[][] letters = new char[boardLines.length][];
+        for (int i = 0; i < boardLines.length; i++) {
+            if (expectedLineLength != boardLines[i].length()) {
+                throw new IllegalArgumentException("The input board is not rectangular");
+            }
+            letters[i] = boardLines[i].toCharArray();
+        }
+
+
+
+
+
+
+
+
         return null;
     }
 }
