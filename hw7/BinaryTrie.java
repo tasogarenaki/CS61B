31c96466c5c8f336c629a6513733f6fb6aba99d2
tasogarenaki <terry.chan2h@gmail.com>
2023-10-27 11:47:32 +0200

    start hw7

---
+++ hw7/BinaryTrie.java
@@ -0,0 +1,10 @@
+
+
+
+
+
+public class BinaryTrie implements Serializable {
+    public BinaryTrie(Map<Character, Integer> frequencyTable)
+    public Match longestPrefixMatch(BitSequence querySequence)
+    public Map<Character, BitSequence> buildLookupTable()
+}
